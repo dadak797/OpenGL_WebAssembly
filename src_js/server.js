6ae16760b78f620c402bbaae5899a693207662b0
dadak797 <dadak797@gmail.com>
2022-04-14 10:47:46 +0900

    add ignore files

--- src_js/server.js
+++ src_js/server.js
@@ -10,5 +10,5 @@ app.use(logger);
 // Serve static files from /public
 app.use( express.static('public') );
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
